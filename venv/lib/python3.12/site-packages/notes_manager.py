20a4122b4ea8246e16c946d5bf9c414bc2f4b262
Tymur Savchuk <stimurr@gmail.com>
2024-11-10 16:11:51 +0200

    updated structure +

---
+++ venv/lib/python3.12/site-packages/notes_manager.py
@@ -0,0 +1,82 @@
+from personal_helper.storage import Storage
+from notes import Note
+from colorama import Fore
+from prettytable import PrettyTable
+
+class NotesManager:
+    def __init__(self):
+        self.notes = Storage.load_notes()
+
+    def add_note(self, title, content, tags=None):
+        new_note = Note(title=title, content=content, tags=tags)
+        self.notes.append(new_note)
+        Storage.save_notes(self.notes)
+        print(Fore.GREEN + "Note successfully added.")
+
+    def edit_note(self, title, field, new_value):
+        note = self.find_note_by_title(title)
+        if note:
+            setattr(note, field, new_value)
+            Storage.save_notes(self.notes)
+            print(Fore.GREEN + f"Note with title '{title}' updated.")
+        else:
+            print(Fore.RED + "Note hasn't been found.")
+
+    def delete_note_by_title(self, title):
+        for note in self.notes:
+            if note.title == title:
+                self.notes.remove(note)
+                Storage.save_notes(self.notes)
+                print(Fore.GREEN + f"Note with title '{title}' deleted.")
+                return
+        print(Fore.RED + f"Note with title '{title}' hasn't been found.")
+
+    def find_note_by_title(self, title):
+        for note in self.notes:
+            if note.title == title:
+                return note
+        print(Fore.RED + f"Note with title '{title}' hasn't been found.")
+        return None
+
+    def display_all_notes(self):
+        if not self.notes:
+            print(Fore.RED + "No notes to display.")
+            return
+
+        table = PrettyTable()
+        table.field_names = ["Title", "Content", "Tags"]
+        for note in self.notes:
+            tags_formatted = ", ".join(note.tags) if note.tags else "Nothing..."
+            table.add_row([note.title, note.content, tags_formatted])
+        print(table)
+
+    def find_notes_by_tag(self, tag):
+        # Фільтруємо нотатки за тегом
+        results = [note for note in self.notes if tag in note.tags]
+        
+        if results:
+            # Відображаємо результати у вигляді таблиці
+            table = PrettyTable()
+            table.field_names = ["Title", "Content", "Tags"]
+            for note in results:
+                tags_formatted = ", ".join(note.tags) if note.tags else "Nothing..."
+                table.add_row([note.title, note.content, tags_formatted])
+            print(Fore.GREEN + f"Notes with the following tag '{tag}' found:")
+            print(table)
+        else:
+            print(Fore.RED + f"No notes with tag '{tag}' found.")
+    
+    def sort_notes_by_tag(self):
+        sorted_notes = sorted(self.notes, key=lambda note: note.tags[0].lower() if note.tags else "")
+        
+        if sorted_notes:
+            # Відображення відсортованих нотаток у вигляді таблиці
+            table = PrettyTable()
+            table.field_names = ["Title", "Content", "Tags"]
+            for note in sorted_notes:
+                tags_formatted = ", ".join(note.tags) if note.tags else "Немає"
+                table.add_row([note.title, note.content, tags_formatted])
+            print(Fore.GREEN + "Notes sorted by tags:")
+            print(table)
+        else:
+            print(Fore.RED + "No notes to sort.")
---
+++ venv/lib/python3.12/site-packages/storage.py
@@ -0,0 +1,37 @@
+import json
+from notes import Note
+from colorama import Fore
+
+NOTES_FILE = "notes.json"
+
+
+class Storage:
+    @staticmethod
+    def save_notes(notes, filename=NOTES_FILE):
+        data = [
+            {
+                "title": note.title,
+                "content": note.content,
+                "tags": note.tags
+            }
+            for note in notes
+        ]
+        with open(filename, "w") as f:
+            json.dump(data, f, indent=4)
+        print(Fore.GREEN + "Note successfully stored.")
+
+    @staticmethod
+    def load_notes(filename=NOTES_FILE):
+        try:
+            with open(filename, "r") as f:
+                data = json.load(f)
+                # Перевірка ключів даних для відладки
+                print("Notes data loaded:", data)
+                # Переконайтеся, що ключі 'title', 'content' і 'tags' існують
+                return [Note(**note_data) for note_data in data]
+        except FileNotFoundError:
+            print(Fore.RED + "No JSON file with notes found. Creating a new file...")
+            return []
+        except TypeError as e:
+            print(Fore.RED + f"JSON format error: {e}")
+            return []
